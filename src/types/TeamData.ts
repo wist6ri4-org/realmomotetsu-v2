44b42b74ba3f226bced62920466d0c91ba3f87c2
Shiro Goto <sg1106.footballdrummer.51046@gmail.com>
2025-07-09 01:25:38 +0900

    docs: :memo: TeamData.tsにコメントを追加

--- src/types/TeamData.ts
+++ src/types/TeamData.ts
@@ -1,5 +1,16 @@
 import { TransitStations } from "@/generated/prisma/client";
 
+/**
+ * チームデータの型定義
+ * @property {number} id - チームのID
+ * @property {string} teamCode - チームコード
+ * @property {string} teamName - チーム名
+ * @property {string} teamColor - チームカラー
+ * @property {TransitStations[]} transitStations - チームの接続駅の配列
+ * @property {number} remainingStationsNumber - 残りの駅数
+ * @property {number} points - ポイント
+ * @property {number} scoredPoints - 得点済みポイント
+ */
 export type TeamData = {
     id: number;
     teamCode: string;
@@ -7,6 +18,6 @@ export type TeamData = {
     teamColor: string;
     transitStations: TransitStations[];
     remainingStationsNumber: number;
-    chargedPoints: number;
-    notChargedPoints: number;
+    points: number;
+    scoredPoints: number;
 };
