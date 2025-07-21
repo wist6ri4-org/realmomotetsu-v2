bf497cae2ba969d60650927ce89159fdff032ebb
Shiro Goto <sg1106.footballdrummer.51046@gmail.com>
2025-07-21 13:01:54 +0900

    docs: :memo: 20250721 ドキュメント修正

--- src/features/transit-stations/types.ts
+++ src/features/transit-stations/types.ts
@@ -1,7 +1,7 @@
 import { TransitStationsGroupedByTeamCode } from "@/types/TransitStationsGroupedByTeamCode";
 
 /**
- * 目的駅の取得リクエスト
+ * 経由駅の取得リクエスト
  * @param eventCode - イベントコード
  */
 export type GetTransitStationsRequest = {
@@ -9,12 +9,12 @@ export type GetTransitStationsRequest = {
 };
 
 /**
- * 目的駅の取得レスポンス
+ * 経由駅の取得レスポンス
  */
 export type GetTransitStationsResponse = TransitStationsGroupedByTeamCode;
 
 /**
- * 目的駅の登録リクエスト
+ * 経由駅の登録リクエスト
  * @param eventCode - イベントコード
  * @param teamCode - チームコード
  * @param stationCode - 駅コード
