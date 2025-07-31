6ac0e3297cf94e3b52ba3c0df0c30a2cd36a4f46
Shiro Goto <sg1106.footballdrummer.51046@gmail.com>
2025-07-31 21:56:24 +0900

    refactor: :recycle: 20250731 #2 テンプレートファイル名を定数化/テンプレートの最適化

---
+++ src/constants/discordNotificationTemplates.ts
@@ -0,0 +1,11 @@
+/**
+ * Discord通知テンプレート名
+ */
+export class DiscordNotificationTemplates {
+    // 目的駅到着時通知
+    static readonly ARRIVAL_GOAL_STATION = "arrivalGoalStation.txt";
+    // ボンビー設定時通知
+    static readonly REGISTER_BOMBII = "registerBombii.txt";
+    // 目的駅設定時通知
+    static readonly REGISTER_GOAL_STATION = "registerGoalStation.txt";
+}
